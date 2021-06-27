c71adc042695ab8f2836ee48284e2b14f8328dae
sunnam sai jyothi swaroop <42674406+saiswaroop5225@users.noreply.github.com>
2021-06-27 10:28:54 +0530

    Create Main.cpp
    basic template

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class University{
+	public:
+		int Teacher_id;
+		int Student_id;
+		int Employee_id;
+		
+		
+}; 
+int main(){
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	University s1;
+	s1.Employee_id=101;
+	cout<<s1.Employee_id;
+	return 0;
+}
